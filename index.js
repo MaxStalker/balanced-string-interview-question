3fa053f314a83cd1ba07d62ad960359374c26b1b
Max Daunarovich <max.daunarovich@gmail.com>
2020-01-07 15:16:17 +0300

    Refactor index.js file a bit. Move methods back and forth to easier read the code

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const roundAndSquareInvalid_1 = "[(])";
 // let's start with method, which will go one character at a time and simply collect number
 // of open contexts
 
-const accumulateStack = (input, trackers) => {
+const stackAccumulator = (input, trackers) => {
   const { isOpen, isClosing, isOpposite } = trackers;
   let stack = [];
 
@@ -67,7 +67,6 @@ const accumulateStack = (input, trackers) => {
       }
     }
   }
-
   return stack;
 };
 
@@ -80,14 +79,6 @@ const roundTrackers = {
     return false;
   }
 };
-
-console.log("Check simple cases");
-const case1 = accumulateStack(simpleValid_1, roundTrackers);
-const case2 = accumulateStack(simpleInvalid_1, roundTrackers);
-const case3 = accumulateStack(simpleInvalid_2, roundTrackers);
-console.log(case1);
-console.log(case2);
-console.log(case3);
 
 const roundAndSquareTrackers = {
   isOpen: chr => "([".includes(chr),
@@ -108,13 +99,21 @@ const roundAndSquareTrackers = {
   }
 };
 
+console.log("Check simple cases");
+const case1 = stackAccumulator(simpleValid_1, roundTrackers);
+const case2 = stackAccumulator(simpleInvalid_1, roundTrackers);
+const case3 = stackAccumulator(simpleInvalid_2, roundTrackers);
+console.log(case1);
+console.log(case2);
+console.log(case3);
+
 console.log("Slightly more complex");
-const case4 = accumulateStack(roundAndSquareValid_1, roundAndSquareTrackers);
-const case5 = accumulateStack(roundAndSquareInvalid_1, roundAndSquareTrackers);
+const case4 = stackAccumulator(roundAndSquareValid_1, roundAndSquareTrackers);
+const case5 = stackAccumulator(roundAndSquareInvalid_1, roundAndSquareTrackers);
 console.log(case4);
 console.log(case5);
 
-const case6 = accumulateStack(valid_2, roundAndSquareTrackers);
+const case6 = stackAccumulator(valid_2, roundAndSquareTrackers);
 console.log(case6);
 
 const splitInput = input => {
@@ -128,16 +127,22 @@ const splitInput = input => {
 const compareParts = (leftStack, rightStack, trackers) => {
   // If stacks are of different size - then it's not balanced
   // Since more context were open or closed
-  console.log("Compare stacks", leftStack, rightStack);
   if (leftStack.length !== rightStack.length) {
     return false;
   }
 
-  const { length } = leftStack;
-  for (let i = 0; i < length; i++) {}
-
   const { isOpposite } = trackers;
-  return false;
+  const reversedRight = rightStack.reverse();
+  const { length } = leftStack;
+
+  for (let i = 0; i < length; i++) {
+    const leftCursor = leftStack[i];
+    const rightCursor = reversedRight[i];
+    if (!isOpposite(leftCursor, rightCursor)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Now let's start with "serial" solution
@@ -145,20 +150,96 @@ const isBalanced = (input, trackers) => {
   const { left, right } = splitInput(input);
 
   // here we will be able to parallelize process in the future
-  const leftStack = accumulateStack(left, trackers);
-  const rightStack = accumulateStack(right, trackers);
+  const leftStack = stackAccumulator(left, trackers);
+  const rightStack = stackAccumulator(right, trackers);
 
   return compareParts(leftStack, rightStack, trackers);
 };
 
-console.clear();
+console.log("\n");
 console.log("Check our splitting method");
-const checkBalanced1 = isBalanced(")(()))", roundTrackers);
+
+const testMethod1 = "((()))";
+console.log(`Checking:, "${testMethod1}"`);
+const checkBalanced1 = isBalanced(testMethod1, roundTrackers);
 console.log(checkBalanced1.toString());
+console.log("\n");
+
+const testMethod2 = "()()()()";
+console.log(`Checking:, "${testMethod2}"`);
+const checkBalanced2 = isBalanced(testMethod2, roundTrackers);
+console.log(checkBalanced2.toString());
+console.log("\n");
+
+const testMethod3 = "[(])";
+console.log(`Checking:, "${testMethod3}"`);
+const checkBalanced3 = isBalanced(testMethod3, roundAndSquareTrackers);
+console.log(checkBalanced3.toString());
+console.log("\n");
+
+const asyncStackAccumulator = async (input, trackers) => {
+  const { isOpen, isClosing, isOpposite } = trackers;
+  let stack = [];
+
+  const inputLength = input.length;
+  for (let i = 0; i < inputLength; i++) {
+    const cursor = input[i];
+    if (isOpen(cursor)) {
+      stack.unshift(cursor);
+      continue;
+    }
+
+    if (isClosing(cursor)) {
+      const lastContext = stack[0];
+      if (isOpposite(cursor, lastContext)) {
+        stack.shift();
+      } else {
+        stack.unshift(cursor);
+      }
+    }
+  }
+  return Promise.resolve(stack);
+};
+
+// Let's make "parallel" solution now
+const asyncIsBalanced = async (input, trackers, delay) => {
+  const { left, right } = splitInput(input);
+  const [leftStack, rightStack] = await Promise.all([
+    asyncStackAccumulator(left, trackers),
+    asyncStackAccumulator(right, trackers)
+  ]);
+  return new Promise(resolve => {
+    const result = compareParts(leftStack, rightStack, trackers);
+    setTimeout(() => {
+      resolve(result);
+    }, delay);
+  });
+};
+
+
+const runHelper = async (input, trackers, delay = 0) => {
+  const result_2 = await asyncIsBalanced(input, trackers, delay);
+  console.log(`Async Result: for "${input}" ->  ${result_2.toString()}`);
+};
+
+// Check async method
+const runAsync = async () => {
+  runHelper("Case 1: )))) lots of closing", roundAndSquareTrackers, 30);
+  runHelper("Case 2: ()() valid string", roundAndSquareTrackers, 5);
+  runHelper(
+    "Case 3: longer string which occasionally have have () and [] brackets" +
+      " here and there!",
+    roundAndSquareTrackers
+  );
+  runHelper("Case 4: ()() valid string", roundAndSquareTrackers, 5);
+};
+
+runAsync();
 
 // Food for thought for more optimization/parallelization:
 //
-// 1) I belive it's possible to async iteration over stacks inside of "compareParts" method
+// 1) I believe it's possible to async iteration over stacks inside of
+// "compareParts" method
 // 2) Split string into more pieces (multiple of 2) - 4, 8
 // 3) Method can be refactored to be used for arrays instead of strings,
 //   thus allowing more complex cases, i.e. HTML tags
